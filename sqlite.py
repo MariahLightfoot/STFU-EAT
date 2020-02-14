eda75e6b23a46c6cbf30a790439b624a8a414ce6
Mariah Lightfoot <mariahlightfoot@Mariahs-MacBook-Pro.local>
2020-02-14 13:57:42 -0600

    added lines to database

--- sqlite.py
+++ sqlite.py
@@ -7,7 +7,11 @@ def create_table():
     c.execute('CREATE TABLE IF NOT EXISTS local_stl_restuarants(id INT, name TEXT)')
 
 def data_entry():
-    c.execute("INSERT INTO local_stl_restuarants VALUES(1, 'hello')")
+    c.execute("INSERT INTO local_stl_restuarants VALUES(1, 'Lonas LiL Eats')")
+    c.execute("INSERT INTO local_stl_restuarants VALUES(2, 'Mochi Cafe')")
+    c.execute("INSERT INTO local_stl_restuarants VALUES(3, 'Southwest Market')")
+    c.execute("INSERT INTO local_stl_restuarants VALUES(4, 'Macs Local Eats')")
+    c.execute("INSERT INTO local_stl_restuarants VALUES(5, 'Copper Pig')")
     conn.commit()
     c.close()
     conn.close()
