5090b420fae8a1dc8b03718c47009c4cfc44b3db
Mariah Lightfoot <mariahlightfoot@Mariahs-MacBook-Pro.local>
2020-02-14 16:01:02 -0600

    can create database and query while in sqlite file, but thinking about how to get result page to load random choice

--- app.py
+++ app.py
@@ -1,21 +1,15 @@
-import sqlite3, config
+import sqlite3
 from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-def connect_db():
-    return sqlite3.connect(config.STFU-EAT.db)
-
 @app.route('/')
 def index():
-    db_connection = connect_db()
-    cursor = db_connection.execute('SELECT id, name, image FROM local-stl-restaurants;')
     return render_template('index.html')
 
-@app.route('/result', methods=['GET', 'POST'])
+@app.route('/result')
 def result():
-    if request.method == 'GET':
-        return render_template('result.html')
+    return render_template('result.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
--- sqlite.py
+++ sqlite.py
@@ -4,17 +4,19 @@ conn = sqlite3.connect('STFUEAT.db')
 c = conn.cursor()
 
 def create_table():
-    c.execute('CREATE TABLE IF NOT EXISTS local_stl_restuarants(id INT, name TEXT, image BLOB)')
+    c.execute('CREATE TABLE IF NOT EXISTS local_stl_restaurants(id INT, name TEXT, image TEXT)')
 
 def data_entry():
-    c.execute("INSERT INTO local_stl_restuarants VALUES(1, 'Lonas LiL Eats', 'lonas-lil-eats.jpg')")
-    c.execute("INSERT INTO local_stl_restuarants VALUES(2, 'Mochi Cafe', 'mochi-cafe.jpg')")
-    c.execute("INSERT INTO local_stl_restuarants VALUES(4, 'VP Square', 'vp-sqaure.jpg')")
-    c.execute("INSERT INTO local_stl_restuarants VALUES(3, 'Southwest Market', 'southwest-market.jpg')")
-    c.execute("INSERT INTO local_stl_restuarants VALUES(5, 'Copper Pig', 'copper-pig.jpg')")
+    c.execute("INSERT INTO local_stl_restaurants VALUES(1, 'Lonas LiL Eats', '/static/images/lonas-lil-eats.jpg')")
+    c.execute("INSERT INTO local_stl_restaurants VALUES(2, 'Mochi Cafe', '/static/images/mochi-cafe.jpg')")
+    c.execute("INSERT INTO local_stl_restaurants VALUES(4, 'VP Square', '/static/images/vp-sqaure.jpg')")
+    c.execute("INSERT INTO local_stl_restaurants VALUES(3, 'Southwest Market', '/static/images/southwest-market.jpg')")
+    c.execute("INSERT INTO local_stl_restaurants VALUES(5, 'Copper Pig', '/static/images/copper-pig.jpg')")
     conn.commit()
-    c.close()
-    conn.close()
 
 create_table()
 data_entry()
+
+c.execute("SELECT * FROM local_stl_restaurants WHERE name='Lonas LiL Eats'")
+print(c.fetchone())
+
