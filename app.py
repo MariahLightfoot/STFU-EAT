a685b142958f4241d76d935d48f58ee81910356e
Mariah Lightfoot <mariahlightfoot@Mariahs-MacBook-Pro.local>
2020-02-14 11:47:43 -0600

    when button is clicked, user is taken to result page

--- app.py
+++ app.py
@@ -1,9 +1,14 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/result', methods=['GET', 'POST'])
+def result():
+    if request.method == 'GET':
+        return render_template('result.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
