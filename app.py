bd2d92cba519946cd93c4c30f240d9312b984b37
Mariah Lightfoot <mariahlightfoot@Mariahs-MacBook-Pro.local>
2020-02-16 21:20:09 -0600

    figured out how to display text/image on results page. now need to get data from db to display

--- app.py
+++ app.py
@@ -4,24 +4,27 @@ from Restaurant import *
 
 app = Flask(__name__)
 
+name = "lonas"
+image= "/static/images/lonas-lil-eats.jpg"
+
 @app.route('/')
 def index():
     return render_template('index.html')
 
 @app.route('/result')
 def result():
-    return render_template('result.html')
+    return render_template('result.html', name=name, image=image)
 
 # if __name__ == '__main__':
 #     app.run(debug=True)
 
-# create_table()
+create_table()
 
-# lonas = Restaurant("Lonas LiL Eats", "/static/images/lonas-lil-eats.jpg") 
+lonas = Restaurant("Lonas LiL Eats", "/static/images/lonas-lil-eats.jpg") 
 # vpSqaure = Restaurant("VP Square", "vp-sqaure.jpg")
 # copperPig = Restaurant("Copper Pig", "copper-pig.jpg")
 # mochiCafe = Restaurant("Mochi Cafe", "mochi-cafe.jpg")
 # southwestMarket = Restaurant("Southwest Market", "southwest-market.jpg")
 
-# data_entry(lonas)
+data_entry(lonas)
 
